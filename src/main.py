22b0bc7627722f9aa878da67b686eefe990125f0
Racso1624 <fer10_lb@outlook.com>
2023-02-15 12:22:40 -0600

    code changes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# Laboratorio 2
+# Laboratorio 3
 # Inteligencia Artificial
 # Authors: YongBum Park 20117, Santiago Taracena 20017, Pedro Arriola 20188, Oscar López 20679
 
--- src/test_train.py
+++ src/test_train.py
@@ -1,3 +1,7 @@
+# Laboratorio 3
+# Inteligencia Artificial
+# Authors: YongBum Park 20117, Santiago Taracena 20017, Pedro Arriola 20188, Oscar López 20679
+
 import random
 import re
 
